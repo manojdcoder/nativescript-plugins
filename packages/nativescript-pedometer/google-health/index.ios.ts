dedff2a57c582e233edb5d9d5f3bb7d0293d84cc
Manojkumar <manojdcoder@gmail.com>
2024-09-08 11:43:54 -0400

    fix: empty functions on ios to avoid crash

--- packages/nativescript-pedometer/google-health/index.ios.ts
+++ packages/nativescript-pedometer/google-health/index.ios.ts
@@ -1,3 +1,21 @@
+import { PedometerData } from '../common';
+
+export function init() {
+  // Not implemented
+}
+
 export function isAvailable(): Promise<boolean> {
   return Promise.resolve(false);
+}
+
+export function isAuthorized(): Promise<boolean> {
+  return Promise.resolve(false);
+}
+
+export function requestAuthorization(): Promise<void> {
+  return Promise.reject();
+}
+
+export function query(): Promise<PedometerData> {
+  return Promise.resolve(null);
 }
